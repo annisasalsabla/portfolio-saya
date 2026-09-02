f7fd50132f7637506eb078b815a929092b811f38
Annisa Salsabila <nisasalsabila23050505@gmail.com>
2026-09-03 02:31:55 +0700

    fix: remove unused imports

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import Tilt from 'react-parallax-tilt';
 import './Certificates.css';
 
 const Certificates = () => {
